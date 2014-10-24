b700be7c158d31daee0c9b245e78e78e5683e997
Nainesh Patel <nainesh.patel@hotmail.co.nz>
2014-10-24 15:25:06 +1300

    Completed problem 2

--- ProjectEuler/src/Main.java
+++ ProjectEuler/src/Main.java
@@ -2,18 +2,24 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		problem2();
 	}
 	
-	public void problem2(){
+	public static void problem2(){
 		int previous = 1;
 		int current = 2;
+		int evenNumbersSum = 0;
 		
-		while(current < 4000000){
+		while(previous < 4000000){
 			int sum = previous + current;
+			if(previous%2 == 0){
+				evenNumbersSum += previous;
+			}
 			previous = current;
 			current = sum;
 		}
+		
+		System.out.println(evenNumbersSum);
 	}
 
 }
